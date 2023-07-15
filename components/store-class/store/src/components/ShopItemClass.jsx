d716151618162e30e873aa354514a9bf7d566dd3
Senibratov S <nlo_tomsk@mail.ru>
2023-07-15 17:50:27 +0700

    work

---
+++ components/store-class/store/src/components/ShopItemClass.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './css/main.css'
+
+export default class ShopItemClass extends Component {
+
+  render() {
+    
+    const { item } = this.props;
+
+    return <div class="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">{item.descriptionFull}</div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+              <div class="price">{item.price}{item.currency}</div>
+              <button>Добавить в корзину</button>
+            </div>
+          </div>
+  }
+}
